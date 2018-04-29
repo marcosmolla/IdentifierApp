bd773a630877ad7ea5c0905112c68e1e9d44361f
marcosmolla <ms2070@me.com>
2018-04-29 11:38:23 -0400

    Initial Commit

---
+++ IdentifierApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  IdentifierApp
+//
+//  Created by Marco Smolla on 29.04.18.
+//  Copyright © 2018 Marco Smolla. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ IdentifierApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  IdentifierApp
+//
+//  Created by Marco Smolla on 29.04.18.
+//  Copyright © 2018 Marco Smolla. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
